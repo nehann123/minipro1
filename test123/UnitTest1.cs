609faa789a9e93128f27d40693bf2a01a2b29ed1
nehann123 <nalageneha99@gmail.com>
2021-07-28 14:40:29 +0530

    test done

---
+++ test123/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using minipro1;
+
+namespace test123
+{
+    public class Tests
+    {
+        [TestFixture]
+        public void UnitTest1()
+        {
+            
+        }
+
+        [Test]
+ 
+        {
+            Assert.Pass();
+        }
+    }
+}
