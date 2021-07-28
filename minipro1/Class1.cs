c3d4569d01169f24da8d4b73e839edab827c97a8
arnab212 <70711617+arnab212@users.noreply.github.com>
2021-07-28 10:41:49 +0530

    AR

--- minipro1/Class1.cs
+++ minipro1/Class1.cs
@@ -9,5 +9,16 @@ namespace minipro1
     class Class1
     {
         Console.WriteLine("Muskan Here");
+         public void AR1()
+        {
+            Console.WriteLine("kamm khatam1");
+        }
+        public void AR2()
+        {
+            Console.WriteLine("kamm khatam2");
+        }public void AR3()
+        {
+            Console.WriteLine("kamm khatam3");
+        }
     }
 }
